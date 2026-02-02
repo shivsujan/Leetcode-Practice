f3ef6bc641a5c9bdba943fce48af4c3f75006b28
shivsujan <145633245+shivsujan@users.noreply.github.com>
2026-02-02 20:02:42 +0530

    Time: 0 ms (100%), Space: 47.7 MB (76.82%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double pow(double x, long n) {
+        if (n==0) return 1;
+        if (n<0) return pow(1/x, -n);
+        if (n%2==0) return pow(x*x, n/2);
+        return pow(x*x, (n-1)/2) * x;
+        
+    }
+
+    public double myPow(double x, int n) {
+        return pow(x, (long)n);
+    }
+}
